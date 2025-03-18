07461a9f90f7b6823854e686ebccb4e576fcaeb4
Dan <mai.d.khanh@gmail.com>
2025-03-17 19:03:42 -0700

    add sphere

--- raytracing/sphere.h
+++ raytracing/sphere.h
@@ -9,6 +9,7 @@ class sphere : public hittable {
 public:
     sphere(point3 _center, double _radius, shared_ptr<material> _material)
         : center(_center), radius(_radius), mat(_material) {}
+
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = r.origin() - center;
         auto a = r.direction().length_squared();
