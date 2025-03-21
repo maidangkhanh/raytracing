d252f8481aa63608bca7455488f66281ca974e07
Dan <mai.d.khanh@gmail.com>
2025-03-20 21:41:59 -0700

    new img

--- develop/main.cpp
+++ develop/main.cpp
@@ -20,7 +20,7 @@ color ray_color(const ray& r) {
 
    vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(1.0, 0.2, 0.3);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(1.0, 0.12, 0.15);
 }
 
 int main() {
