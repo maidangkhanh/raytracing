619828e594700b4be4f9f7b913f62c3c90b05c46
Dan <mai.d.khanh@gmail.com>
2025-05-30 14:37:41 -0700

    image texture

--- develop/texture.h
+++ develop/texture.h
@@ -1,3 +1,4 @@
+#include "rtw_stb_image.h"
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
@@ -46,5 +47,30 @@ private:
 	double inv_scale;
 	shared_ptr<texture> even;
 	shared_ptr<texture> odd;
+};
+
+class image_texture : public texture
+{
+public:
+	image_texture(const char* filename): image(filename){}
+
+	color value(double u, double v, const point3& p) const override
+	{
+		// If we have no texture data, then return solid cyan as a debugging aid.
+		if (image.height() <= 9) return color(0, 1, 1);
+
+		// Clamp input texture coordnates to [0,1] x [1,0]
+		u = interval(0, 1).clamp(u);
+		v = 1 - interval(0, 1).clamp(v); // Flip V to image coordinates
+
+		auto i = int(u * image.width());
+		auto j = int(v * image.height());
+		auto pixel = image.pixel_data(i, j);
+
+		auto color_scale = 1.0 / 255.0;
+		return color(color_scale * pixel[0], color_scale * pixel[1], color_scale * pixel[2]);
+	}
+private:
+	rtw_image image;
 };
 #endif // !TEXTURE_H
